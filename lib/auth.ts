b697abfb8b2e7f71fe4d6e186b62d907877377b9
Chris Gambrell <christopher@gambrell.info>
2024-10-05 17:01:57 -0400

    Add prisma and nextauth

---
+++ lib/auth.ts
@@ -0,0 +1,36 @@
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import NextAuth, { NextAuthConfig } from 'next-auth'
+import prisma from './db'
+
+export const authConfig = {
+	adapter: PrismaAdapter(prisma),
+	callbacks: {
+		authorized: ({ auth, request: { nextUrl } }) => {
+			const isAuthed = !!auth?.user
+			const isAuthRoute =
+				// TODO: Forgot page
+				nextUrl.pathname.startsWith('/login') || nextUrl.pathname.startsWith('/register') || nextUrl.pathname.startsWith('/forgot')
+
+			if (!isAuthRoute) {
+				if (isAuthed) return true
+				return false
+			} else if (isAuthed) return Response.redirect(new URL('/', nextUrl))
+			return true
+		},
+	},
+	pages: { signIn: '/login' },
+	providers: [],
+	session: { strategy: 'jwt' },
+} satisfies NextAuthConfig
+
+export const { handlers, auth: session, signIn, signOut } = NextAuth(authConfig)
+
+export const auth = async () => {
+	const session = await NextAuth(authConfig).auth()
+	if (!session?.user) throw new Error('Not authenticated.')
+
+	const user = await prisma.user.findFirst({ where: { email: session.user.email ?? '' } })
+	if (!user) throw new Error('User not found')
+
+	return user
+}
